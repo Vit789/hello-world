181bd09c45673dec0275968b3269872d41860d23
Vit789 <49677522+Vit789@users.noreply.github.com>
2021-10-16 23:26:31 +0300

    Update Calc_functions.py
    Update Calc_functions.py from file in computer by copy-paste.
    Idea to move all needed to separate functions:
    -MainWorkflow
    -CheckOperation
    -InputNumber
    -ExecOperation

--- Calc_functions.py
+++ Calc_functions.py
@@ -1,5 +1,6 @@
-def inputNumber(textForInput):
+def InputNumber(textForInput):
 ### inputNumber gets number and check is it int or float or not a number
+### returning number: 0=correct\1=incorrect, 
     inputNum = input (textForInput)
     if inputNum.isnumeric():
         # print("toInt")
@@ -11,54 +12,71 @@ def inputNumber(textForInput):
         return 0, inputNum
     else:
         #print("it is not a number")
-        return 1
+        return 1, 0
     
+def ExecOperation(operationAsked, a, b):
+### ExecutOperation
+    Vars = {}
+    operationExec = """res = {}""".format(operationAsked).replace("a", str(a)).replace("b", str(b))
+    exec(operationExec, Vars)
+    return 0, Vars['res']
 
 
+def CheckOperation(operationInput):
+    if operationInput == "+":
+        return 0, "a+b"    
+    elif operationInput == "-":
+        return 0, "a-b"
+    elif operationInput == "-a":
+        return 0, "-a"
+    elif operationInput == "%":
+        return 0, "a%b"
+    elif operationInput == "*":    
+        return 0, "a*b"
+    elif operationInput == "/":    
+        return 0, "a/b"
+    else:
+        return 1, 0
 
-what = input("Operation (+, -, -a, %, *, /): ")
-operationAsked = "" # nothing
-if what == "+":
-    operationAsked = "a+b"    
-elif what == "-":
-    operationAsked = "a-b"
-elif what == "-a":
-    operationAsked = "-a"
-elif what == "%":
-    operationAsked = "a%b"
-elif what == "*":    
-    operationAsked = "a*b"
-elif what == "/":    
-    operationAsked = "a/b"
-else:
-    print("Wrong operation")
+def MainWorkflow():
+### Later it would be main in module
+    chooseOperation = input("Operation (+, -, -a, %, *, /): ")
+    operationAsked = "" # nothing
+    checkOperationValid, operationAsked = CheckOperation(chooseOperation)
+    if checkOperationValid == 1:
+        print("Wrong operation")
+        return
+    countNumbersInput = 0
+    for x in operationAsked:
+        if x in ["a", "b"]:
+            countNumbersInput += 1
+    print (countNumbersInput)
+    a = 0
+    res = 0
+    checkNumberValid = 0
+    checkNumberValid, res = InputNumber("Input first number: ")
+    if checkNumberValid == 0:
+        a = res
+    else:
+        print("a is not a number, Finish him")
+        return 1, 0
+    b = 0
+    res = 0
+    checkNumberValid = 0
+    if countNumbersInput > 1:
+        checkNumberValid, res = InputNumber("Input second number: ")
+        if checkNumberValid == 0:
+            b = res
+        else:
+            print("b is not a number, Finish him")
+            return 1, 0
+    checkOperationValid = 0
+    checkOperationValid, result = ExecOperation(operationAsked, a, b)
+    if checkOperationValid == 1:
+        print("Wrong operation")
+        return 1, 0
+    print ("result of operation is: {}".format(str(result)))
+    print ("end")
+    return 0, result
 
-countNumbersInput = 0
-for x in operationAsked:
-    if x in ["a", "b"]:
-        countNumbersInput += 1
-print (countNumbersInput)
-
-a = 0
-res = inputNumber("Input first number: ")
-if res[0] == 0:
-    a = res[1]
-else: print("a is not a number, Finish him")
-
-b = 0
-if countNumbersInput > 1:
-    resB = inputNumber("Input second number: ")
-    if resB[0] == 0:
-        b = resB[1]
-    else: print("b is not a number, Finish him")
-
-
-Vars = {}
-operationExec = """res = {}""".format(operationAsked).replace("a", str(a)).replace("b", str(b))
-exec(operationExec, Vars)
-
-result = 0
-result = Vars['res']
-print ("result of operation is: {}".format(str(result)))
-print ("end")
-
+MainWorkflow()
